9034c6dc2a7ad9190cdf752f99076bac9525a7da
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-12-09 17:32:38 +0900

    create seeder for users table

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +15,36 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => '1',
+            ],
+
+            // Instructor
+            [
+                'name' => 'Instructor',
+                'username' => 'instructor',
+                'email' => 'instructor@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'instructor',
+                'status' => '1',
+            ],
+
+            // User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1',
+            ],
+        ]);
     }
 }
