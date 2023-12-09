4a24003200f2c0e25881f18b0742e7c17fa58d7e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-12-09 23:08:36 +0900

    create admin and instructor routes, pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
