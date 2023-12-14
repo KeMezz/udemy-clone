9c7b1531dff349b4001f28563f5b9bc23ad5c7b1
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-12-14 21:53:02 +0900

    filled with real data

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -10,7 +10,7 @@
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">User Profilep</li>
+                        <li class="breadcrumb-item active" aria-current="page">User Profile</li>
                     </ol>
                 </nav>
             </div>
@@ -25,12 +25,14 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex flex-column align-items-center text-center">
-                                    <img src="{{ asset('backend/assets/images/avatars/avatar-2.png') }}" alt="Admin"
-                                         class="rounded-circle p-1 bg-primary" width="110">
+                                    <img
+                                        src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('/upload/no_image.jpg') }}"
+                                        alt="Admin"
+                                        class="rounded-circle p-1 bg-primary" width="110">
                                     <div class="mt-3">
-                                        <h4>John Doe</h4>
-                                        <p class="text-secondary mb-1">Full Stack Developer</p>
-                                        <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                                        <h4>{{ $profileData->name }}</h4>
+                                        <p class="text-secondary mb-1">{{ $profileData->username }}</p>
+                                        <p class="text-muted font-size-sm">{{ $profileData->email }}</p>
                                         <button class="btn btn-primary">Follow</button>
                                         <button class="btn btn-outline-primary">Message</button>
                                     </div>
@@ -61,10 +63,20 @@
                             <div class="card-body">
                                 <div class="row mb-3">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Full Name</h6>
+                                        <h6 class="mb-0">Name</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="John Doe"/>
+                                        <input type="text" name="name" class="form-control"
+                                               value="{{ $profileData->name }}"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">User Name</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="text" class="form-control" name="username"
+                                               value="{{ $profileData->username }}"/>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -72,7 +84,8 @@
                                         <h6 class="mb-0">Email</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="john@example.com"/>
+                                        <input type="text" class="form-control" name="email"
+                                               value="{{ $profileData->email }}"/>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -80,15 +93,8 @@
                                         <h6 class="mb-0">Phone</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(239) 816-9029"/>
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Mobile</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(320) 380-4539"/>
+                                        <input type="text" name="phone" class="form-control"
+                                               value="{{ $profileData->phone }}"/>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -96,8 +102,26 @@
                                         <h6 class="mb-0">Address</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control"
-                                               value="Bay Area, San Francisco, CA"/>
+                                        <input type="text" class="form-control" name="address"
+                                               value="{{ $profileData->address }}"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                        <h6 class="mb-0">Profile Image</h6>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <input type="file" class="form-control" name="photo"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-sm-3">
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        <img
+                                            src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('/upload/no_image.jpg') }}"
+                                            alt="Admin"
+                                            class="rounded-circle p-1 bg-primary" width="80">
                                     </div>
                                 </div>
                                 <div class="row">
