b8cc7c116126839fe9c23a3ca2bf5af9f3325c60
Hyeongjin <88423849+KeMezz@users.noreply.github.com>
2024-01-01 22:35:18 +0900

    create instructor profile page

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,5 +27,41 @@ class InstructorController extends Controller
     public function InstructorLogin()
     {
         return view('instructor.instructor_login');
+    }
+
+    public function InstructorProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('instructor.instructor_profile_view', compact('profileData'));
+    }
+
+    public function InstructorProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images/'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = [
+            'message' => 'Instructor Profile Updated Successfully',
+            'alert-type' => 'success',
+        ];
+
+        return redirect()->back()->with($notification);
     }
 }
--- resources/views/instructor/body/header.blade.php
+++ resources/views/instructor/body/header.blade.php
@@ -384,7 +384,7 @@
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item d-flex align-items-center" href="{{ route('admin.profile') }}"><i
+                    <li><a class="dropdown-item d-flex align-items-center" href="{{ route('instructor.profile') }}"><i
                                 class="bx bx-user fs-5"></i><span>Profile</span></a>
                     </li>
                     <li><a class="dropdown-item d-flex align-items-center"
