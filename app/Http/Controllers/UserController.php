1cbeb77d0dcd5a41adc86d231a9a47cbe3ac50ee
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-02-04 22:58:48 +0900

    implements user logout from sidebar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -19,6 +20,17 @@ class UserController extends Controller
         $profileData = User::find($id);
 
         return view('frontend.dashboard.edit_profile', compact('profileData'));
+    } // End Method
+
+    public function UserLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     } // End Method
 
     public function UserProfileUpdate(Request $request)
--- resources/views/frontend/dashboard/body/sidebar.blade.php
+++ resources/views/frontend/dashboard/body/sidebar.blade.php
@@ -67,7 +67,7 @@
             Reviews</a></li>
 
 
-    <li><a href="index.html">
+    <li><a href="{{ route('user.logout') }}">
             <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px">
                 <path d="M0 0h24v24H0V0z" fill="none"/>
                 <path
